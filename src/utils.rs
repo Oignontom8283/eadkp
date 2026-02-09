647c09c34928a749bd6d9858fb9450056201cc4f
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-02-09 00:18:37 +0000

    refactor: move memory utils to utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
 
+use super::*;
+use core::ptr;
+
 /// Magic number for EIF1 format. Magic number in hex `0x31464945`
 pub const EIF1_MAGIC_NUMBER: u32 = u32::from_le_bytes(*b"EIF1"); 
 
@@ -61,3 +64,68 @@ pub struct Point {
     pub y: u16,
 }
 
+
+// ============================================================================
+// MEMORY UTILITIES
+// ============================================================================
+
+/// Compare deux C strings
+pub unsafe fn strcmp(s1: *const u8, s2: *const u8) -> bool {
+    let mut p1 = s1;
+    let mut p2 = s2;
+
+    while unsafe { *p1 != 0 && *p1 == *p2 } { // Comparer jusqu'au null terminator ou différence
+        // Avancer les pointeurs
+        p1 = unsafe { p1.add(1) };
+        p2 = unsafe { p2.add(1) };
+    }
+    unsafe { ((*p1 as i32) - (*p2 as i32)) == 0 } // Différence ASCII, si 0, ce sont les mêmes caractères, donc on est arrivé à la fin des deux chaînes en même temps
+
+}
+
+/// Copie n bytes de src vers dest (zones non chevauchantes)
+/// 
+/// **Erreur en cas de chevauchement des zones mémoire** pour éviter les comportements indéfinis/corrompuptions.
+#[cfg(target_os = "none")]
+pub unsafe fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> Result<(), SoftwareError> {
+    
+    if n == 0 {
+        return Ok(()); // Rien à copier, pas de risque de chevauchement
+    }
+
+    let src_start = src as usize;
+    let src_end = src_start.checked_add(n).ok_or(SoftwareError::PointerOverflow)?;
+    let dest_start = dest as usize;
+    let dest_end = dest_start.checked_add(n).ok_or(SoftwareError::PointerOverflow)?;
+
+    // Vérifier que les zones ne chevauchent pas
+    if dest_start < src_end && src_start < dest_end {
+        // Zones chevauchantes détectées
+        return Err(SoftwareError::OverlappingRegions {
+            src_start: src,
+            src_end: src.add(n),
+            dest_start: dest,
+            dest_end: dest.add(n)
+        });
+    }
+    
+    // Effectuer la copie
+    ptr::copy_nonoverlapping(src, dest, n);
+    Ok(())
+}
+
+/// Copie `n` bytes de `src:*` vers `dest:*` (zones peuvent chevaucher)
+/// 
+/// **Comportement défini même en cas de chevauchement:** La copie se fait par une mémoire tampon.
+#[cfg(target_os = "none")]
+pub unsafe fn memmove(dest: *mut u8, src: *const u8, n: usize) {
+    unsafe { ptr::copy(src, dest, n) }
+}
+
+/// Remplit n bytes avec la valeur c
+#[cfg(target_os = "none")]
+pub unsafe fn memset(s: *mut u8, c: u8, n: usize) {
+    for i in 0..n {
+        unsafe { *s.add(i) = c };
+    }
+}
