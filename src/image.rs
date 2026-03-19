1faabebfa4e02890b10935800fc4ff52667d0fd7
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-03-19 22:50:50 +0000

    refactor: streamline image handling by introducing PixelsSource trait and optimizing memory usage

--- src/image.rs
+++ src/image.rs
@@ -1,103 +1,83 @@
 
 use super::*;
-use alloc::boxed::Box;
+use alloc::{boxed::Box, slice};
 
-
-/// # Représente une image chargée en mémoire
-/// 
-/// ## Champs
-/// - `magic_number`: Numéro magique identifiant le format de l'image
-/// - `width`: Largeur de l'image en pixels d'écran (axe Y)
-/// - `height`: Hauteur de l'image en pixels d'écran (axe X)
-/// - `pixels`: Données des pixels de l'image au format Color (RGB565)
-/// - `binary`: Pointeur vers les données binaires brutes de l'image dans la flash (Immuable/Statique)
-/// 
-/// # Remarques
-/// 
-/// Utilisation de Box au lieu de Vec pour une allocation fixe. Économie de mémoire !
-///
-/// Explication :
-/// ```rust
-/// // Pour l'object Vec<T> :
-/// ptr: *mut T, // 8 octets
-/// len: usize,  // 8 octets
-/// cap: usize,  // 8 octets
-///              // Total: 24 octets
-/// 
-/// // Pour l'object Box<[T]> :
-/// ptr: *mut T, // 8 octets
-/// len: usize,  // 8 octets
-///              // Total: 16 octets
-/// ```
-/// 
-/// Économie de 8 octets (64 bits) par image chargée en mémoire. C'est considérable sur plusieurs images !
-///
-pub struct Image {
-    pub magic_number: u32,
-    pub width: u16,
-    pub height: u16,
-    pub pixels: Box<[Color]>,
-    pub binary: &'static [u8],
+/// Source de données pour les pixels d'une image. Peut être en flash ou en RAM.
+pub trait PixelsSource {
+    fn buffer(&self) -> &[u8];
 }
 
-impl Image {
-    pub fn from_raw(binary_raw: &'static [u8]) -> Option<Self> {
-        
-        // Lire le magic number 
-        // let magic_number: u32 = unsafe { *(binary_raw.as_ptr() as *const u32) };
-        let magic_number: u32 = u32::from_le_bytes(
-            binary_raw[..4].try_into().expect("Slice trop courte pour magic number")
-        );
-
-        // EIF1 format
-        if magic_number == utils::EIF1_MAGIC_NUMBER {
-            return Some(Image::from_raw_eif1(binary_raw));
-        };
-        
-        // Format inconnu
-        None
+/// Source de pixels en flash. Lent mais pas de stockage en RAM nécessaire.
+pub struct FlashSource(pub &'static [u8]);
+impl PixelsSource for FlashSource {
+    fn buffer(&self) -> &[u8] {
+        self.0
     }
-    
-    pub fn from_raw_eif1(binary_raw: &'static [u8]) -> Self {
-        // Lire le magic number (déréférencer pour obtenir la valeur)
-        let magic_number: u32 = unsafe { *(binary_raw.as_ptr() as *const u32) };
+}
 
-        // Lire width et height (déréférencer pour obtenir les valeurs)
-        let width: u16 = unsafe { *(binary_raw.as_ptr().add(4) as *const u16) };
-        let height: u16 = unsafe { *(binary_raw.as_ptr().add(6) as *const u16) };
-        let surface_size = (width as usize) * (height as usize);
+/// Source de pixels en RAM. Rapide mais consomme de la RAM pour stocker les données de l'image.
+pub struct RamSource(pub Box<[u8]>);
+impl PixelsSource for RamSource {
+    fn buffer(&self)-> &[u8] { &self.0 }
+}
 
-        // Obtenir un pointeur vers les données binaires de l'image
-        let binary: &'static [u8] = &binary_raw[8..];
 
-        // Créer directement le Box<[Color]> avec un iterator
-        let pixels: Box<[Color]> = (0..surface_size)
-            .map(|i| {
-                let idx = i * 2;
-                let rgb565 = u16::from_le_bytes([binary[idx], binary[idx + 1]]);
-                Color { rgb565 }
-            })
-            .collect();
+pub trait ImageFormat {
+    /// Récupérer la largeur de l'image en pixels.
+    fn width(&self) -> u16;
+    /// Récupérer la hauteur de l'image en pixels.
+    fn height(&self) -> u16;
+    /// Dessiner l'image a l'écran a une postion donnée
+    fn draw(&self, dest: Point);
+}
 
-        Image {
-            magic_number,
-            width,
-            height,
-            pixels,
-            binary,
-        }
+/// Format d'image "EIF1" (EADKP Image Format 1).
+/// - Pas de transparence
+/// - Pas de compression
+/// - Couleurs au format RGB565 (16 bits par pixel)
+/// 
+/// | Nom           | Taille                     |  
+/// | ------------- | -------------------------- |
+/// | Magic Number  | 4 bytes                    |
+/// | Width         | 2 bytes                    |
+/// | Height        | 2 bytes                    |
+/// | Pixels (data) | (width * height * 2) bytes |
+pub struct Eif1<S: PixelsSource> {
+    pub width: u16,
+    pub height: u16,
+    pub source: S,
+}
+impl<S: PixelsSource> ImageFormat for Eif1<S> {
+    fn width(&self) -> u16 {
+        self.width
     }
 
-    pub fn get_pixels(&self) -> &[Color] {
-        &self.pixels
+    fn height(&self) -> u16 {
+        self.height
     }
 
-    pub fn for_coordinates(&self, x: u16, y: u16) -> Rect {
-        Rect {
-            x,
-            y,
+    fn draw(&self, _dest: Point) {
+
+        // Construire le rectangle de destination pour l'affichage
+        let rect = Rect {
+            x: _dest.x,
+            y: _dest.y,
             width: self.width,
             height: self.height,
-        }
+        };
+
+        // Magic number (4 bytes) + width (2 bytes) + height (2 bytes) = 8 bytes de header
+        let raw_bytes = &self.source.buffer()[(4+2+2)..];
+
+        // Interpréter les pixels comme des Color
+        let pixels: &[Color] = unsafe {
+            slice::from_raw_parts(
+                raw_bytes.as_ptr() as *const Color,
+                raw_bytes.len() / 2
+            )
+        };
+
+        // Envoyer le buffer a l'écran
+        display::push_rect(rect, pixels);
     }
 }
