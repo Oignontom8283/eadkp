fb17645b2e275df3446aa2e8534961bc3174ea1c
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-06-27 22:04:18 +0000

    feat: add Version struct with methods for version creation and parsing

--- src/utils.rs
+++ src/utils.rs
@@ -60,3 +60,48 @@ pub struct Point {
     pub x: u16,
     pub y: u16,
 }
+
+
+/// Représente une version au format "major.minor.patch", par exemple "1.2.3".
+/// 
+/// ## Exemple :
+/// ```
+/// let v =     Version::parse("21.5.6").unwrap();
+/// let v_min = Version::new(17, 1, 0);
+/// let v_max = Version::new(22, 4, 5);
+/// 
+/// assert!(v >= v_min);
+/// assert!(v <= v_max);
+/// assert!(v >= v_min && v <= v_max);
+/// 
+/// ```
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Version {
+    pub major: u8,
+    pub minor: u8,
+    pub patch: u8,
+}
+
+impl Version {
+    /// Crée une nouvelle instance de Version à partir des numéros de version fournis.
+    pub fn new(major: u8, minor: u8, patch: u8) -> Self {
+        Version { major, minor, patch }
+    }
+    
+    /// Créer une instance de Version à partir d'une chaîne de caractères au format "major.minor.patch".
+    /// - Retourne None si la chaîne n'est pas au format attendu.
+    pub fn parse(s: &str) -> Option<Self> {
+        let mut parts = s.split('.');
+
+        let major = parts.next()?.parse::<u8>().ok()?;
+        let minor = parts.next()?.parse::<u8>().ok()?;
+        let patch = parts.next()?.parse::<u8>().ok()?;
+
+        // Si la chaîne ne contient pas exactement trois parties, retourner None
+        if parts.next().is_none() {
+            return None;
+        }
+
+        Some(Self { major, minor, patch })
+    }
+}
