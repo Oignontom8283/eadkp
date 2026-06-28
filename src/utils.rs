7c523112df1972419bc0dfe75d3c1867d45aaffe
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-06-28 22:51:48 +0000

    feat: add ptr_range_size function to calculate size between two pointers

--- src/utils.rs
+++ src/utils.rs
@@ -25,3 +25,23 @@ pub fn str_from_fixed_buffer(buffer: &[u8]) -> Result<&str, GlobalError> {
     // Convertir le slice en &str sans vérifier la validité UTF-8 (unsafe)
     Ok(unsafe { str::from_utf8_unchecked(slice) })
 }
+
+
+/// Calculer la taille d'une plage de mémoire définie par deux pointeurs.
+/// - Renvoie 0 si les pointeurs sont invalides ou dans le mauvais ordre.
+/// 
+/// ## Exemple
+/// ```
+/// let start_ptr = 0x1000 as *const u8; // 4096
+/// let end_ptr = 0x2000 as *const u8; // 8192
+/// 
+/// let size = ptr_range_size(start_ptr, end_ptr);
+/// 
+/// assert_eq!(size, 0x1000); // 4096
+/// ```
+pub unsafe fn ptr_range_size(start: *const u8, end: *const u8) -> usize {
+    if start.is_null() || end.is_null() || end < start {
+        return 0;
+    }
+    end.offset_from(start) as usize
+}
