54a95557c0d8adb9f177bd4211b2e9b838f8dcd2
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-07-10 13:03:49 +0000

    refactor: add shared constants and types for library and compilation script

---
+++ src/shared.rs
@@ -0,0 +1,7 @@
+
+// Ce fichier contient les constantes et les types partagés entre la lib et le script de compilation.
+// Son contenu est automatiquement réexporté par le module `constant` de la lib.
+
+
+/// Magic number for EIF1 format. Magic number in hex `0x31464945`
+pub const EIF1_MAGIC_NUMBER: u32 = u32::from_le_bytes(*b"EIF1"); 
