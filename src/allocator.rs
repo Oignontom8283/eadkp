9ef9c6aae58b12ce9019784ef3d02ff94b81960c
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-07-16 12:39:41 +0000

    feat: initialize allocator sub-module with core dependencies

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,3 +1,9 @@
 /*!
 Sous-module `allocator` pour l'implémentation et gestion de l'allocateur global de heap sur hardware.
 */
+
+use core::sync::atomic::{AtomicBool, Ordering};
+use crate::{GlobalError, SoftwareError};
+
+#[cfg(target_os = "none")]
+use embedded_alloc::LlffHeap as Heap;
